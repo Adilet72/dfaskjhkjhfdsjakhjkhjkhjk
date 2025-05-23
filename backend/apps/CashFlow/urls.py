377e34088be5b5a60fe3453fb5906c136337a4c9
Адилет <adiletashimov08@gmail.com>
2025-05-24 01:46:59 +0600

    Update urls.py

--- backend/apps/CashFlow/urls.py
+++ backend/apps/CashFlow/urls.py
@@ -1,16 +1,6 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import StatusViewSet, OperationTypeViewSet, CategoryViewSet, SubcategoryViewSet, CashFlowViewSet
-
-router = DefaultRouter()
-
-router.register(r'status', StatusViewSet)
-router.register(r'operation-types', OperationTypeViewSet)
-router.register(r'categories', CategoryViewSet)
-router.register(r'subcategories', SubcategoryViewSet)
-router.register(r'cashflows', CashFlowViewSet)
+from django.urls import path
+from .views import SendPromptAPIView
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('send/', SendPromptAPIView.as_view()),
 ]
-
