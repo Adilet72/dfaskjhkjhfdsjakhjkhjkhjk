d112ecb2dadb2876bdf163e29ea961f36454490a
Адилет <adiletashimov08@gmail.com>
2025-06-03 00:58:59 +0600

    Update cors.py

--- backend/config/settings/cors.py
+++ backend/config/settings/cors.py
@@ -23,12 +23,12 @@ CORS_ALLOW_HEADERS = [
 # CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGIN_REGEXES = [
-    r"^http://django-server-production-8abc.up.railway.app$",
-    r"^http://127\.0\.0\.1(:\d+)?$",  # локальный сервер
-    r"^http://localhost(:\d+)?$",  # локальный сервер с localhost
-    r"^https://adilet72\.github\.io/Islam-ai-checkar-ts/?$",  # GitHub Pages
+    r"^https://adilet72\.github\.io/Islam-ai-checkar-ts/?$",  # GitHub Pages точечный
+    r"^https://adilet72\.github\.io$",                       # общий адрес GitHub Pages
+    r"^http://127\.0\.0\.1(:\d+)?$",
+    r"^http://localhost(:\d+)?$",
+    r"^https://django-server-production-8abc.up.railway.app$",
 ]
-
 CSRF_TRUSTED_ORIGINS = [
     'https://django-server-production-8abc.up.railway.app',
     'https://adilet72.github.io',
